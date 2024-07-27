7b482f8f529ae35ef7288ca3327731d2be25ec57
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2024-07-27 23:30:37 +0530

    chat-feat: basic working testing

---
+++ index.js
@@ -0,0 +1,28 @@
+const http = require("http");
+const express = require("express");
+const path = require("path");
+
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+//Socket.io Handling
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+app.use(express.static(path.resolve("./public")));
+
+app.get("/", (req, res) => {
+  return res.sendFile("/public/index.html");
+});
+
+server.listen(9000, () => {
+  console.log(`Server Started At PORT 9000`);
+});
